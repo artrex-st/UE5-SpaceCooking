d9440446ac17aa18d064936715f4f60bbe1b6687
Arthur Stefanelli <artrexwow@gmail.com>
2025-04-02 17:39:34 -0300

    add MPSession SubSystem

---
+++ Source/SpaceCooking/Public/Game/SCGameInstance.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SCGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACECOOKING_API USCGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+public:
+    virtual void Init() override;
+};
